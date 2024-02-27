319cb59f4fafa17ebe400de2a07b48e3d18f7ea6
Georgie Armstrong <georgietree@gmail.com>
2024-02-27 15:44:10 +1100

    fixed sliding panel

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
@@ -7,7 +8,7 @@ import { Badge } from './components/badge'; // Ensure this is correctly imported
 function ExpandingSidePanel({ open, setOpen }) {
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-0" onClose={() => setOpen(false)}>
+      <Dialog as="div" className="relative z-10" onClose={() => setOpen(false)}>
         <Transition.Child
           as={Fragment}
           enter="ease-in-out duration-300"
@@ -17,35 +18,102 @@ function ExpandingSidePanel({ open, setOpen }) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-950 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-gray-900 bg-opacity-60 transition-opacity"/>
         </Transition.Child>
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
               <Transition.Child
                 as={Fragment}
-                enter="transform transition ease-in-out duration-300 sm:duration-500"
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
                 enterFrom="translate-x-full"
                 enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-300 sm:duration-500"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <div className="flex h-full flex-col overflow-y-scroll bg-sky-950 py-6 shadow-xl">
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-xl">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-indigo-950 bg-opacity-25 py-6 shadow-xl">
                     <div className="px-4 sm:px-6">
-                      <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-sky-700">Let's talk</Dialog.Title>
-                        <div className="ml-3 flex h-7 items-center">
+                      <div className="mt-10 flex items-start justify-between">
+                        <Dialog.Title className="text-lg font-semibold text-indigo-500">Let's talk</Dialog.Title>
+                        <div className="mt-10bg-opacity-75 ml-8 flex h-7 items-center">
                           <button
                             onClick={() => setOpen(false)}
-                            className="bg-transparent rounded-md text-sky-400 hover:text-sky-500 focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                            className="bg-transparent rounded-md text-indigo-500 hover:text-sky-500 focus:ring-2 focus:ring-offset-2"
                           >
                             <span className="sr-only">Close panel</span>
                             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                           </button>
                         </div>
                       </div>
+                      {/* Here starts the form */}
+                      <form action="#" method="POST" className="space-y-6 pt-6 pb-5">
+                        <div>
+                          <label htmlFor="first-name" className="block text-sm font-semibold leading-8 text-indigo-300 mt-10 ml-">
+                            First name
+                          </label>
+                          <div className="mt-3">
+                            <input
+                              type="text"
+                              name="first-name"
+                              id="first-name"
+                              autoComplete="given-name"
+                              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-lg"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-indigo-300">
+                            Last name
+                          </label>
+                          <div className="mt-3">
+                            <input
+                              type="text"
+                              name="last-name"
+                              id="last-name"
+                              autoComplete="family-name"
+                              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-lg"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <label htmlFor="email" className="block text-sm font-semibold leading-6 text-indigo-300">
+                            Email
+                          </label>
+                          <div className="mt-3">
+                            <input
+                              type="email"
+                              name="email"
+                              id="email"
+                              autoComplete="email"
+                              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-lg"
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <label htmlFor="message" className="block text-sm font-semibold leading-6 text-indigo-300">
+                            Message
+                          </label>
+                          <div className="mt-3">
+                            <textarea
+                              name="message"
+                              id="message"
+                              rows={8}
+                              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-lg"
+                            />
+                          </div>
+                        </div>
+                        <div className="flex justify-end">
+                          <button
+                            type="submit"
+                            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Send
+                          </button>
+                        </div>
+                      </form>
+                      {/* Here ends the form */}
                     </div>
                   </div>
                 </Dialog.Panel>
@@ -57,6 +125,7 @@ function ExpandingSidePanel({ open, setOpen }) {
     </Transition.Root>
   );
 }
+
 
 export default function Example() {
   // Function to trigger the opening of the side panel
@@ -71,7 +140,7 @@ export default function Example() {
 
       {/* Your existing content */}
   <div>
-  <div className="mx-auto max-w-7xl px-6 pb-10 pt-10 sm:pb-1 lg:flex lg:px-0">
+  <div className="mx-auto max-w-7xl pb-10 pt-10 sm:pb-1 lg:flex lg:px-0">
   <div className="mx-auto max-w-7xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-40">
     <div className="flex">
       <div className="animate-pulse-fast">
@@ -80,7 +149,7 @@ export default function Example() {
       <img className="max-h-28" src="indiauder.png" alt="Kauder"/>
     </div>
           <div className="mt-24 sm:mt-32 lg:mt-16">
-            <a href="#" className="inline-flex space-x-6"> {/* Ensure you add href attribute or use a button for semantics */}
+            <a href="https://gretel.space" className="inline-flex space-x-6"> {/* Ensure you add href attribute or use a button for semantics */}
               <span className="inline-flex items-center space-x-1 text-sm font-medium leading-6 text-gray-300">
                 <ChevronRightIcon className="h-5 w-5 text-indigo-500"/>
                 <div className="flex gap-4">
@@ -97,7 +166,7 @@ export default function Example() {
           End to end solutions happen here
           </h3>
           <p className="mt-6 text-lg leading-8  text-slate-200">
-           A small team with the right skills. From design to deploy - we can build a solution that delivers and delights. Cut out the noise and collaborate directly with creators at Kauder.
+           A big team with the right skills. From design to deploy - we can build a solution that delivers and delights. Cut out the noise and collaborate directly with creators at Kauder.
           </p>
           <div className="mt-10 flex items-center gap-x-6">
           <button 
